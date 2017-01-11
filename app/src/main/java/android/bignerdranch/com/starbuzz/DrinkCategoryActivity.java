778c182bdc62a790c471bb6b6205f2f6a06043ac
slad_den <slad_den@mail.ru>
2017-01-12 00:49:01 +0300

    2) DrinkCategoryActivity pg.282

---
+++ app/src/main/java/android/bignerdranch/com/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,15 @@
+package android.bignerdranch.com.starbuzz;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Denis on 12.01.2017.
+ */
+
+public class DrinkCategoryActivity extends ListActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
