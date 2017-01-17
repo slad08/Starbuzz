04f328c38515399ec4c1d4fc5f5d612223e2858e
slad_den <slad_den@mail.ru>
2017-01-18 00:13:47 +0300

    2) DrinkCategoryActivity pg.282 3)DrinkCatActivity pg 286

--- app/src/main/java/android/bignerdranch/com/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/android/bignerdranch/com/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,11 @@
 package android.bignerdranch.com.starbuzz;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Denis on 12.01.2017.
@@ -11,5 +15,31 @@ public class DrinkCategoryActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ListView listDrinks = getListView();
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        listDrinks.setAdapter(listAdapter);
     }
+
+//
+//    @Override
+//    public void onListItemClick(ListView listView, View itemView, int position, long id) {
+//        Intent intent = new Intent(DrinkCategoryActivity.this,DrinkActivity.class);
+
+    @Override
+    public void onListItemClick(ListView listView,
+                                View itemView,
+                                int position,
+                                long id) {
+   //  super.onListItemClick(l, v, position, id);
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO,(int)id);
+        startActivity(intent);
+    }
+//        intent.putExtra(DrinkActivity.EXTRA_DRINKNO,(int) id);
+//        startActivity(intent);
+//        super.onListItemClick(listView, itemView, position, id);
+//    }
 }
